92b8b1dcdc48a655be4df0d06b287e5f548d1c76
MOFAZZAL HOSEN <35369040+Mofazzal874@users.noreply.github.com>
2023-04-02 08:08:32 +0600

    Update bubbleSort.cpp

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -35,6 +35,48 @@ for 3rd round , swap number - 0 to n- 3 = 3
 using namespace std;
 typedef long long ll;
 
+
+
+class node{
+    public:
+    int info ;
+    node* next ;
+    node(int val){
+        info = val ; 
+        next = NULL; 
+    }  
+
+};
+
+
+
+void lastInsert( node* &head , int data){  
+                                        
+
+    node* n = new node(data);
+    if(head == NULL){   
+        head =n ;       
+        return ;  
+
+    }
+    node *temp = head; 
+
+    while(temp->next !=NULL){
+        temp = temp->next; 
+    } 
+    temp->next = n ; 
+
+}
+
+void display(node* head){
+    node* temp = head ; 
+    while(temp != NULL){
+        cout<<temp->info<<" ";
+        temp = temp->next; 
+    }
+    cout<<"\n" ;
+}
+
 void swp (int &a , int &b){
 
     a = a+b ; 
@@ -52,7 +94,36 @@ void bubbleSort(int a[] , int n){
         }
     }
 }
+/*
+//optimised code for best time complexity :O(n)
+//this is sometimes asked in interview
+lets imagine a case , where the array is already sorted 
+like this ; 2 3 5 7 10 
+then u don't have to run all the round 
 
+or this case : 4 3 2 5 7 10 
+where 4 3 2 is not sorted and after that the array is sorted , so you don't need to swap after that; 
+so you can check if all the swap cases in a round does happens or not . 
+if not , then break the loop and take decision that the array is sorted 
+otherwise simply go by the previous rule 
+*/
+
+void bubbleSortOptimised(int a[] , int n ) {
+    for( int i = 1 ; i <= n-1 ; i++){
+        int didswap = 0 ; 
+        for(int j = 0 ; j <=(n-i) ;j++ ){
+            if(a[j] > a[j+1]){
+                swp(a[j] , a[j+1]) ;
+                didswap = 1;  
+            }
+        }
+        if( didswap == 0){ //this means no swap has happened .after this round , the array is sorted 
+            break; 
+        }
+        cout<<"round: "<<i<<endl; //this will keep track if any round if passed or not 
+    }
+
+}
 
 
 int main()
@@ -62,9 +133,35 @@ int main()
     for(int i = 0 ; i < n ; i++){
         cin>>array[i] ;
     }
-    bubbleSort(array , n) ;
+    bubbleSortOptimised(array , n) ;
     for(int i = 0 ; i < n ; i++){
         cout<<array[i]<<" " ;
     }
+    
+
+
  
 }
+
+/*
+input :
+6
+13 46 24 52 20 9
+output:
+round: 1
+round: 2
+round: 3
+round: 4
+round: 5
+9 13 20 24 46 52
+input:
+6
+1 2 3 4 5 6
+output:
+1 2 3 4 5 6
+input:
+6
+4 1 3 5 6 7
+output:
+round: 1
+1 3 4 5 6 7
